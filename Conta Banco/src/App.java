b164d3c6d40c30fd177e15ead217b6f4f802b6d0
Bruno Silva <bgsnascimento21@gmail.com>
2023-09-13 19:49:49 -0300

    Conta java

--- Conta Banco/src/App.java
+++ Conta Banco/src/App.java
@@ -6,32 +6,36 @@ public class App {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int numero;
+		int numeroConta;
 		String agencia;
 		String nomeCliente;
 		double saldo;
 
-		System.out.println("---------------CONTA BANCÁRIA---------------");
-		System.out.println();
-		System.out.println("Por favor, digite o número da Conta: (1234)");
-		numero = sc.nextInt();
+	
+		System.out.println("Por favor, digite o número da Conta: ");
+		numeroConta = sc.nextInt();
 
-		System.out.println("Agora digite o número da Agência: (123-8)");
+		System.out.println("Agora digite o número da Agência:");
 		agencia = sc.next();
 
 		System.out.println("Me informe seu nome: ");
 		nomeCliente = sc.next();
 		sc.nextLine();
 
-		System.out.println("Agora me informa seu saldo atual: ");
+		System.out.println("informe seu saldo atual: ");
 		saldo = sc.nextDouble();
 
-		System.out.println();
-		System.out.printf("Olá %s, obrigado por criar uma conta em "
-				+ "nosso banco, sua agência é %s, "
-				+ "conta %d e seu saldo %.2f já está disponível"
-				+ " para saque.", nomeCliente, agencia, numero, saldo);
-
+		System.out.println("Seu numero de conta é " + numeroConta);
+		
+		System.out.println("Seu numero de agencia é " + agencia);
+		
+		System.out.println("Seu nome é " + nomeCliente);
+		
+		
+		System.out.println("Seu saldo é " + saldo);
 		sc.close();
+		
+		
+	
     }
 }
